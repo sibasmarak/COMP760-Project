23b925393caad086da800133d1b7abb74ef70cc9
prashant.govindarajan <prashant.govindarajan@login-1.server.mila.quebec>
2022-12-12 23:18:27 -0500

    added perov_super .pt files

--- cdvae/cdvae/pl_data/datamodule.py
+++ cdvae/cdvae/pl_data/datamodule.py
@@ -81,12 +81,16 @@ class CrystDataModule(pl.LightningDataModule):
                 hydra.utils.instantiate(dataset_cfg)
                 for dataset_cfg in self.datasets.val
             ]
-
+            
             self.train_dataset.lattice_scaler = self.lattice_scaler
             self.train_dataset.scaler = self.scaler
+            print('Saving Train Dataset!')
+            torch.save(self.train_dataset, '/home/mila/p/prashant.govindarajan/scratch/COMP760-Project/cdvae/data/perov_super/train_super.pt')
             for val_dataset in self.val_datasets:
                 val_dataset.lattice_scaler = self.lattice_scaler
                 val_dataset.scaler = self.scaler
+            print('Saving Validation Dataset!')
+            torch.save(self.val_dataset, '/home/mila/p/prashant.govindarajan/scratch/COMP760-Project/cdvae/data/perov_super/val_super.pt')
 
         if stage is None or stage == "test":
             self.test_datasets = [
@@ -96,6 +100,8 @@ class CrystDataModule(pl.LightningDataModule):
             for test_dataset in self.test_datasets:
                 test_dataset.lattice_scaler = self.lattice_scaler
                 test_dataset.scaler = self.scaler
+            print('Saving Test Dataset!')
+            torch.save(self.test_dataset, '/home/mila/p/prashant.govindarajan/scratch/COMP760-Project/cdvae/data/perov_super/test_super.pt')
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
